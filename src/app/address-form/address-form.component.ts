287d888a941ad6d237a3952634cc140f6d96ffba
SEBASTIAO LEMOS DE SOUSA JUNIOR <slsousa@trt13.jus.br>
2024-08-06 09:58:51 -0300

    implementa a funcionalidade de forms aninhados

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import {
   AbstractControl,
   ControlValueAccessor,
@@ -8,27 +8,60 @@ import {
   Validator,
   Validators
 } from '@angular/forms';
-import {noop, Subscription} from 'rxjs';
+import { noop, Subscription } from 'rxjs';
 
 @Component({
   selector: 'address-form',
   templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
-})
-export class AddressFormComponent {
-
-    @Input()
-    legend:string;
-
-    form: FormGroup = this.fb.group({
-        addressLine1: [null, [Validators.required]],
-        addressLine2: [null, [Validators.required]],
-        zipCode: [null, [Validators.required]],
-        city: [null, [Validators.required]]
-    });
-
-    constructor(private fb: FormBuilder) {
+  styleUrls: ['./address-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: AddressFormComponent,
+      multi: true
     }
+  ]
+})
+export class AddressFormComponent implements ControlValueAccessor, OnDestroy {
+
+  @Input()
+  legend: string;
+
+  onTouched = () => { };
+
+  onChangeSub: Subscription;
+
+  form: FormGroup = this.fb.group({
+    addressLine1: [null, [Validators.required]],
+    addressLine2: [null, [Validators.required]],
+    zipCode: [null, [Validators.required]],
+    city: [null, [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  writeValue(obj: any): void {
+    if (obj) {
+      this.form.setValue(obj);
+    }
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.form.disable() : this.form.enable();
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeSub = this.form.valueChanges.subscribe(fn);
+  }
+
+  ngOnDestroy(): void {
+    this.onChangeSub.unsubscribe();
+  }
 
 }
 
